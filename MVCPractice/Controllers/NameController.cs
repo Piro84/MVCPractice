4f4dcc565f56799184c0202b02f38da0dfb73957
Thomas <thomasmharmeyer@gmail.com>
2019-03-22 11:02:24 -0500

    Did some stuff?

---
+++ MVCPractice/Controllers/NameController.cs
@@ -0,0 +1,23 @@
+﻿using MVCPractice.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class NameController : Controller
+    {
+        // GET: Name
+        public ActionResult Index()
+        {
+            ViewBag.NameList = "Name lsit";
+            List<NameList> nameList = new List<NameList>()
+            {
+                new NameList{ID=0, FirstName="Thomas", LastName = "Harmeyer"},
+                new NameList { ID = 1, FirstName = "Nabil", LastName = "Hussani" },
+                new NameList { ID = 2, FirstName = "Anja", LastName = "Logan" },
+            };
+
+            return View(nameList);
+        }
+    }
+}
