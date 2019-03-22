4d298ffb4cf2e5489e2ec99c45dad932892220f4
Nirbhay <nirbhayvig43@gmail.com>
2019-03-22 12:12:23 -0500

    array stuff

---
+++ MVCPractice/Controllers/FirstController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPractice.Models;
+
+namespace MVCPractice.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            Employee empl = new Employee();
+            empl.name = "bob";
+            empl.age = 141;
+            empl.Qualifications = new string[] {"c#", "java", "MVC" };
+            return View(viewName: "Index", model: empl);
+        }
+
+        [HttpPost]
+        public ActionResult SayHello(Employee emp)
+        {
+            return View(viewName: "Index", model: emp);
+        }
+
+    }
+}
---
+++ MVCPractice/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPractice.Models
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string[] Qualifications { get; set; }
+    }
+}
