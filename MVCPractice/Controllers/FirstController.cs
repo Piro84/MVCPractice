fa07f414077db4e997b3cf74c6daabee47925bd5
Nirbhay <nirbhayvig43@gmail.com>
2019-03-25 09:42:35 -0500

    yote

--- MVCPractice/Controllers/FirstController.cs
+++ MVCPractice/Controllers/FirstController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MVCPractice.Models;
+using MVCPractice;
 
 namespace MVCPractice.Controllers
 {
@@ -12,17 +12,7 @@ namespace MVCPractice.Controllers
         // GET: First
         public ActionResult Index()
         {
-            Employee empl = new Employee();
-            empl.name = "bob";
-            empl.age = 141;
-            empl.Qualifications = new string[] {"c#", "java", "MVC" };
-            return View(viewName: "Index", model: empl);
-        }
-
-        [HttpPost]
-        public ActionResult SayHello(Employee emp)
-        {
-            return View(viewName: "Index", model: emp);
+            return View();
         }
 
     }
