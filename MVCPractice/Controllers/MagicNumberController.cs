6038b9d70c277fa9130e95099c36b277db8ca606
Thomas <thomasmharmeyer@gmail.com>
2019-03-25 10:48:25 -0500

    Magic Number

---
+++ MVCPractice/Controllers/MagicNumberController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class MagicNumberController : Controller
+    {
+        // GET: MagicNumber
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MagicNumberSubmit(int firstNumber, int secondNumber)
+        {
+            Random random = new Random();
+            return View(viewName: "Index", model: random.Next(firstNumber,secondNumber));
+        }
+    }
+}
