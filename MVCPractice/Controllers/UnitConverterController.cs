1154431e8e1b5d7997ef9d655af669087a2da926
Thomas <thomasmharmeyer@gmail.com>
2019-03-26 13:46:50 -0500

    Did all of the unit conversion

---
+++ MVCPractice/Controllers/UnitConverterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class UnitConverterController : Controller
+    {
+        // GET: UnitConverter
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model:"");
+        }
+
+        public ActionResult Convert(double toConvert, String UnitIn, String UnitOutWithComma)
+        {
+            string[] outStringArray = UnitOutWithComma.Split(',');
+            string outputString = outStringArray[1];
+            string UnitOut = outStringArray[0];
+            Double.TryParse(UnitIn, out double input);
+            Double.TryParse(UnitOut, out double output);
+            toConvert = input * toConvert / output;
+            return View(viewName: "Index", model: toConvert + " " + outputString);
+        }
+    }
+}
